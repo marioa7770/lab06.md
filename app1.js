594a039aa31660262c6b47e101962de2386ece68
mario acevedo <jun_acevedo415@yahoo.com>
2021-10-12 20:47:43 -0700

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,20 @@
+console.log('Hello World!')
+
+let userName = prompt('What is your Name?')
+
+let catType = prompt('What is your favorite cat breed')
+
+
+if(userName == 'Mario'){
+    document.write('<h2>Welcome Catmaster' + userName + '</h2>')
+} else {
+    document.write('<h2>Welcome' + userName + '</h2>')
+}
+
+
+// Data Types
+
+// string: Letters or numbers
+// numbers or integer
+// Boolean True or False
+// Null
